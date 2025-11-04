b66d704923470433ff03114047b85a421e07df9e
Rick1330 <133630952+Rick1330@users.noreply.github.com>
2025-11-04 20:39:02 +0300

    Signed-off-by: Rick1330 <elishum8@gmail.com>

---
+++ apps/web/src/lib/metrics.ts
@@ -0,0 +1,20 @@
+import client from 'prom-client';
+
+const register = new client.Registry();
+
+client.collectDefaultMetrics({ register });
+
+export const httpRequestDurationMs = new client.Histogram({
+  name: 'http_request_duration_ms',
+  help: 'Duration of HTTP requests in ms',
+  labelNames: ['route', 'method', 'status'] as const,
+  buckets: [50, 100, 200, 300, 500, 1000, 2000],
+});
+
+register.registerMetric(httpRequestDurationMs);
+
+export async function metricsText(): Promise<string> {
+  return await register.metrics();
+}
+
+export { register };
